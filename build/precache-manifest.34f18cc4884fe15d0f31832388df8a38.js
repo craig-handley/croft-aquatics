5e999fbb210bccfd3a655715e6f0fb16ec8e4cce
clicktravel-craig <craig.handley@clicktravel.com>
2020-05-15 10:50:21 +0100

    config for heroku

---
+++ build/precache-manifest.34f18cc4884fe15d0f31832388df8a38.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "810d289caf6d071d4f15396f6cb2d66d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "895323ed2f7258af4fae2c738c8aea49",
+    "url": "/jquery.min.js"
+  },
+  {
+    "revision": "f89065e3d988006af9791b44561d7c90",
+    "url": "/jquery.scrollex.min.js"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "92b5506255e0a563d53c3aa37e612861",
+    "url": "/main.js"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "63eae98d84a04c4ce52e92787d861838",
+    "url": "/skel.min.js"
+  },
+  {
+    "revision": "ef2c76afcfa059e0e5f2",
+    "url": "/static/css/main.48f3457e.chunk.css"
+  },
+  {
+    "revision": "ef2c76afcfa059e0e5f2",
+    "url": "/static/js/main.ef2c76af.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "48b1e95b4909634351f2",
+    "url": "/static/js/vendors~main.48b1e95b.chunk.js"
+  },
+  {
+    "revision": "e3a055e3cb8c4b2fb4b8b51dfb56b925",
+    "url": "/static/media/bg.e3a055e3.jpg"
+  },
+  {
+    "revision": "722c2ae04bdf317fd9b4cbe74741c63a",
+    "url": "/static/media/pic01.722c2ae0.jpg"
+  },
+  {
+    "revision": "18ded4dce7a6d56acf80b0d8439aec9c",
+    "url": "/static/media/pic02.18ded4dc.jpg"
+  },
+  {
+    "revision": "7549a9e7ab5c9dd3fe55111ba6f187e9",
+    "url": "/static/media/pic03.7549a9e7.jpg"
+  },
+  {
+    "revision": "10cd20684e8efc2d263657a41c2b2b85",
+    "url": "/static/media/pic04.10cd2068.jpg"
+  },
+  {
+    "revision": "bdf45f9b008c6cbe67a68932d66f4f9f",
+    "url": "/static/media/slide01.bdf45f9b.jpg"
+  },
+  {
+    "revision": "a392d3ea3ab1fcc606cb502845df85b1",
+    "url": "/static/media/slide02.a392d3ea.jpg"
+  },
+  {
+    "revision": "6c10a716e9039ccf2598e0996b2612bf",
+    "url": "/static/media/slide03.6c10a716.jpg"
+  },
+  {
+    "revision": "dd37d51496598c0224144a7a0107fd1c",
+    "url": "/static/media/slide04.dd37d514.jpg"
+  },
+  {
+    "revision": "1ad8a27a4c666abf929edbc1f9fa4fc7",
+    "url": "/static/media/slide05.1ad8a27a.jpg"
+  },
+  {
+    "revision": "fd2716a7b68ce7748c9676787b61db43",
+    "url": "/util.js"
+  }
+]);
